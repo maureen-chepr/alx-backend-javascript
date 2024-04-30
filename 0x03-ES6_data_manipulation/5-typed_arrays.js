1b1978f94b4de6acbc4f2f8e421b0df9f909c896
root <maureenchep95@gmail.com>
2024-04-30 21:17:46 +0300

    fixed lint

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,7 +2,7 @@ export default function createInt8TypedArray(length, position, value) {
   if (position >= length) {
     throw new Error('Position outside range');
   }
-  const n_int8Array = new Int8Array(length);
-  n_int8Array.fill(value, position, position + 1);
-  return new DataView(n_int8Array.buffer);
+  const nint8Array = new Int8Array(length);
+  nint8Array.fill(value, position, position + 1);
+  return new DataView(nint8Array.buffer);
 }
